0bb70011ba71ae7b7566d0e683584e94b323650b
Juan Andres Orozco <juanxxi2015@gmail.com>
2024-08-28 14:53:31 -0500

    Update some stuff

--- src/main/java/com/example/demo/controller/UrlController.java
+++ src/main/java/com/example/demo/controller/UrlController.java
@@ -2,8 +2,12 @@ package com.example.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.UrlDto;
 import com.example.demo.model.Url;
@@ -30,5 +34,9 @@ public class UrlController {
     @Operation(summary = "Recuperar URL original", description = "Recupera la URL original a partir de una URL acortada.")
     public Url getOriginalUrl(@PathVariable String shortUrl) {
         return urlService.getOriginalUrl(shortUrl);
+    }
+    @GetMapping("/status")
+    public String getStatus() {
+        return "Up and Running";
     }
 }
